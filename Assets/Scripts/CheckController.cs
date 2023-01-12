0e76fbed9e137a334a1e235ece5fd82e0e974d5d
Pasquier Baptiste <baptiste.pasquier8@gmail.com>
2023-01-12 13:41:35 +0100

    Test p2 Controllers

--- Assets/Scripts/CheckController.cs
+++ Assets/Scripts/CheckController.cs
@@ -5,51 +5,14 @@ using UnityEngine.XR;
 
 public class CheckController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public List<GameObject> controllerPrefabs;
-    private InputDevice targetDeviceRight;
-    private InputDevice targetDeviceLeft;
-
-    private GameObject spawnedController;
-
-    void Start()
+    private void Update()
     {
-        List<InputDevice> devices = new List<InputDevice>();
-        //right controller
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
 
-
-        if (devices.Count > 0)
+        foreach (var device in inputDevices)
         {
-            targetDeviceRight = devices[0];
-            spawnedController = Instantiate(controllerPrefabs[0], transform);
-
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
         }
-        //left controller
-        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
-        if (devices.Count > 0)
-            targetDeviceLeft = devices[0];
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //bouton
-
-        if (targetDeviceRight.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
-            Debug.Log("Pressing primary button");
-
-        //gachette
-
-        if (targetDeviceRight.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
-            Debug.Log("Triger pressed" + triggerValue);
-
-        //joystick
-
-        if (targetDeviceRight.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2SAxisValue) && primary2SAxisValue != Vector2.zero)
-            Debug.Log("Primary touchpad" + primary2SAxisValue);
     }
 }
